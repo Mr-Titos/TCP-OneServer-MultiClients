6be8a3458b856934ff0f2045b34b5700aa9913f7
Titos <arthur.titos@gmail.com>
2019-03-14 21:19:15 +0100

    Init repos

---
+++ ServerTcp/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+
+namespace ServerTcp
+{
+    class Program
+    {
+        const string SERVER_IP = "127.0.0.1";
+        byte[] bytes = new byte[1024];
+        //ArrayList readListe = new ArrayList();
+        static List<Socket> readList = new List<Socket>();
+        static Socket ServerSocket = new Socket(AddressFamily.InterNetwork,
+                      SocketType.Stream,
+                      ProtocolType.Tcp);
+
+        static int port = 0;
+        static IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+
+        static byte[] saucisse = new byte[99999];
+
+        static void Main(string[] args)
+        {
+            Console.Write("Port : ");
+            port = Convert.ToInt32(Console.ReadLine());
+
+            Thread start;
+            start = new Thread(Start);
+            start.Start();
+
+            Thread list;
+            list = new Thread(Listen);
+            list.Start();
+
+        }
+
+        public static void Listen()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                List<Socket> temp = readList;
+                Thread.Sleep(10);
+                foreach (Socket client in temp)
+                {
+                    try
+                    {
+                        if (!client.Connected)
+                            continue;
+
+                        string txt = "";
+                        while (client.Available > 0)
+                        {
+                            byte[] bytes = new byte[client.ReceiveBufferSize];
+                            int byteRec = client.Receive(bytes);
+                            saucisse = bytes;
+                            if (byteRec > 0)
+                                txt += Encoding.UTF8.GetString(bytes, 0, byteRec);
+
+                            if (!string.IsNullOrEmpty(txt))
+                            {
+                                Console.WriteLine(client.RemoteEndPoint + " : " + txt);
+                                byte[] salt = Encoding.UTF8.GetBytes(txt);
+                                foreach(Socket so in readList)
+                                    so.Send(salt, salt.Length, SocketFlags.None);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+
+        public static void Start()
+        {
+            IPHostEntry ipHostEntry = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostEntry.AddressList[0];
+            Console.WriteLine("IP=" + ipAddress.ToString());
+            Socket CurrentClient = null;
+            
+            try
+            {
+                ServerSocket.Bind(new IPEndPoint(localAdd, port));
+                //Le 10 veut dire 10 clients max
+                ServerSocket.Listen(10);
+
+                while (true)
+                {
+                    CurrentClient = ServerSocket.Accept();
+                    Console.WriteLine("Nouveau client:" + CurrentClient.RemoteEndPoint);
+                    readList.Add(CurrentClient);
+                }
+            }
+            catch (SocketException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+    }
+}
+        
---
+++ TryTCP/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TryTCP
+{
+    namespace Client
+    {
+        class Program
+        {
+            static Socket ClientSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Adresse IP : ");
+                String SERVER_IP = Console.ReadLine();
+                Console.WriteLine("Port : ");
+                int port = Convert.ToInt32(Console.ReadLine());
+
+                Thread Listen;
+                Listen = new Thread(Receive);
+                Listen.Start();
+      
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = IPAddress.Parse(SERVER_IP);
+
+                ClientSocket.Connect(ipAddress,port);
+
+                while (true)
+                {
+                    Console.Write("Quoi envoyer ?  -- ");
+                    string textToSend = Console.ReadLine();
+
+                    byte[] bytesToSend = Encoding.ASCII.GetBytes(textToSend);
+
+                    Console.WriteLine("Sending : " + textToSend);
+                    ClientSocket.Send(bytesToSend,bytesToSend.Length,SocketFlags.None);
+                    Thread.Sleep(100);
+                    
+                }
+            }
+
+            static void Receive()
+            {
+                while (Thread.CurrentThread.IsAlive)
+                {
+                    if (!ClientSocket.Connected)
+                        continue;
+
+                    byte[] bytesToRead = new byte[ClientSocket.ReceiveBufferSize];
+
+                    byte[] msg = new Byte[ClientSocket.Available];
+                    ClientSocket.Receive(msg, 0, ClientSocket.Available, SocketFlags.None);
+
+                    String messageReceived = Encoding.UTF8.GetString(msg);
+                    if (messageReceived.Length > 0)
+                        Console.WriteLine("Recu : " + messageReceived );
+                }
+            }
+
+        }
+    }
+
+}
